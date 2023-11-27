549ecabf23df13ff93bb281dc786a8a885113f5c
Denis Kulik <kulikdenis1996x@gmail.com>
2023-11-27 20:33:25 +0700

    lesson 1: work with cycles

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -4,3 +4,24 @@ for (let i = 0; i < arr.length; i++) {
     const el = arr[i]
     console.log(el)
 }
+
+for (let i = arr.length - 1 ; i >= 0; i--) {
+    console.log(arr[i])
+}
+
+for (let i = 0; i < arr.length; i++) {
+    const el = arr[i]
+    if(i === arr.length - 1) {
+        console.log(el, 'no next element')
+    } else {
+        const nextEl = arr[i + 1]
+        console.log(el, nextEl)
+    }
+}
+
+let index;
+while (index < arr.length) {
+    const el = arr[index]
+    console.log(el)
+    index++
+}
