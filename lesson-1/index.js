2b78269adde80a63cf340a30448beb7cf27a85fb
Denis Kulik <kulikdenis1996x@gmail.com>
2023-12-01 21:25:57 +0700

    lesson 1: find min and max

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,3 +1,5 @@
+// for
+
 const arr = [1, 2, 3]
 
 for (let i = 0; i < arr.length; i++) {
@@ -5,13 +7,13 @@ for (let i = 0; i < arr.length; i++) {
     console.log(el)
 }
 
-for (let i = arr.length - 1 ; i >= 0; i--) {
+for (let i = arr.length - 1; i >= 0; i--) {
     console.log(arr[i])
 }
 
 for (let i = 0; i < arr.length; i++) {
     const el = arr[i]
-    if(i === arr.length - 1) {
+    if (i === arr.length - 1) {
         console.log(el, 'no next element')
     } else {
         const nextEl = arr[i + 1]
@@ -19,9 +21,26 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
+// while
+
 let index;
 while (index < arr.length) {
     const el = arr[index]
     console.log(el)
     index++
+}
+
+// Find min and max
+const numbers = [1, 4, 9, 16, -1, 25]
+
+let min = numbers[0]
+let max = numbers[0]
+
+for (let i = 1; i < numbers.length; i++) {
+    const number = numbers[i]
+    if (number < min) {
+        min = number
+    } else if (number > max) {
+        max = number
+    }
 }
